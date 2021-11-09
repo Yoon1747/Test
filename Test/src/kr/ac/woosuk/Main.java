93152f9410714d54771f0369794f71ea5125e57b
Yoon1747 <93562354+Yoon1747@users.noreply.github.com>
2021-11-09 10:13:00 +0900

    abcd

--- Test/src/kr/ac/woosuk/Main.java
+++ Test/src/kr/ac/woosuk/Main.java
@@ -1,10 +1,14 @@
 package kr.ac.woosuk;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		pizza = pizzaStore.orderPizza("pepperoni");
+		System.out.println(pizza+" °í°´ Àü´Þ\n");
+		
+		pizza = pizzaStore.orderPizza("clam");
+		System.out.println(pizza+" °í°´ Àü´Þ\n");
 
 	}
-
 }
